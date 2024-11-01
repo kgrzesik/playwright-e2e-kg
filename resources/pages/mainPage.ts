1ceb544d4e2f00f891885b1c58d3bddd6655f982
kgrzesik <56387772+kgrzesik@users.noreply.github.com>
2024-11-01 23:35:02 +0100

    sort-test (#4)

--- resources/pages/mainPage.ts
+++ resources/pages/mainPage.ts
@@ -2,6 +2,7 @@ import { expect, Page } from "@playwright/test";
 import { BasePage } from "./basePage";
 import { MainPageLocators } from "../locators/mainPageLocators";
 import { SidebarButtons } from "../utilities/dictionaries/sidebarButtons";
+import { SortOptions } from "../utilities/dictionaries/sortOptions";
 
 export class MainPage extends BasePage {
   locators: MainPageLocators;
@@ -12,11 +13,10 @@ export class MainPage extends BasePage {
   }
 
   //Public functions
-  public async logout(option: SidebarButtons) {
+  public async chooseOptionFromSide(option: SidebarButtons) {
     //Open sidebar
     await this.locators.burgerButton.click();
 
-    //Logout
     switch (option) {
       case SidebarButtons.ALL_ITEMS:
         await this.locators.sidebarButton("inventory").click();
@@ -29,6 +29,62 @@ export class MainPage extends BasePage {
         break;
       case SidebarButtons.RESET_APP_STATE:
         await this.locators.sidebarButton("reset").click();
+        break;
+    }
+  }
+
+  public async chooseSortOption(option: SortOptions) {
+    await this.locators.sortContainer.selectOption(option);
+  }
+
+  public async checkSortingOnPage(option: SortOptions) {
+    switch (option) {
+      case SortOptions.NAME_A_TO_Z:
+        {
+          //Get all product names
+          const allProductsNames: string[] = await this.locators.productNames.allTextContents();
+          //Sort table by name a to z
+          const sortedProducts = [...allProductsNames].sort((a, b) => a.localeCompare(b));
+          // Checking if values are the same
+          expect(allProductsNames).toEqual(sortedProducts);
+        }
+        break;
+
+      case SortOptions.NAME_Z_TO_A:
+        {
+          //Get all product names
+          const allProductsNames: string[] = await this.locators.productNames.allTextContents();
+          //Sort table by name z to a
+          const sortedProducts = [...allProductsNames].sort((a, b) => b.localeCompare(a));
+          // Checking if values are the same
+          expect(allProductsNames).toEqual(sortedProducts);
+        }
+        break;
+      case SortOptions.PRICE_HIGHT_TO_LOW:
+        {
+          //Get all product prices
+          const allProductsPricesString: string[] = await this.locators.productPrices.allTextContents();
+          const allProductsPrices: number[] = allProductsPricesString.map((allProductsPricesString) => parseFloat(allProductsPricesString.replace("$", "")));
+
+          //Sort table by price to hight to low
+          const sortedProducts = allProductsPrices.sort((a, b) => b - a);
+
+          // Checking if values are the same
+          expect(allProductsPrices).toEqual(sortedProducts);
+        }
+        break;
+      case SortOptions.PRICE_LOW_TO_HIGH:
+        {
+          //Get all product prices
+          const allProductsPricesString: string[] = await this.locators.productPrices.allTextContents();
+          const allProductsPrices: number[] = allProductsPricesString.map((allProductsPricesString) => parseFloat(allProductsPricesString.replace("$", "")));
+
+          //Sort table by price to low to hight
+          const sortedProducts = allProductsPrices.sort((a, b) => a - b);
+
+          // Checking if values are the same
+          expect(allProductsPrices).toEqual(sortedProducts);
+        }
         break;
     }
   }
